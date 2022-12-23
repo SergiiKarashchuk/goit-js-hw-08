79f7819e6e6d6a678533b7673a150a868630e691
Сергей Каращук <kspost01@gmail.com>
2022-12-23 02:30:40 +0200

    draft version

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,49 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+console.log(SimpleLightbox);
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+// console.log(galleryItems);
+
+// звертаюсь до галереї що є масивом об'єктів
+const galleryContainer = document.querySelector('.gallery');
+
+// створюю змінну що є строками карток масиву
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+// створюю контейнер і додаю в нього всі картки галереї що є строками
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
+
+
+console.log(createGalleryCardsMarkup(galleryItems));
+
+// функція заборони стандартних дій щоб браузер не відкривав картинку у іншому вікні
+// function blockStandartAction(evt) {
+//     evt.preventDefault();
+// }
+
+
+// Створюю розмітку для картки як шаблон
+function createGalleryCardsMarkup(galleryItems)  {
+    return galleryItems.map(({ preview, original, description }) => {
+       return `
+       <a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `; 
+    }).join('');
+  }
+
+  
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', onFormData);
+form.addEventListener('submit', onSubmitForm);
 
+
+function onFormData(evt) {
+
+}
+
+function onSubmitForm(evt) {
+
+}
