08dca72a14c04e99a2e22f8b7b84164d359b9fdc
Сергей Каращук <kspost01@gmail.com>
2022-12-24 19:11:39 +0200

    for check

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,16 +4,10 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(SimpleLightbox);
-
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
-
-// console.log(galleryItems);
 
 // звертаюсь до галереї що є масивом об'єктів
 const galleryContainer = document.querySelector('.gallery');
+
 
 // створюю змінну що є строками карток масиву
 const cardsMarkup = createGalleryCardsMarkup(galleryItems);
@@ -21,16 +15,7 @@ const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 // створюю контейнер і додаю в нього всі картки галереї що є строками
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
-
-
-console.log(createGalleryCardsMarkup(galleryItems));
-
-// функція заборони стандартних дій щоб браузер не відкривав картинку у іншому вікні
-// function blockStandartAction(evt) {
-//     evt.preventDefault();
-// }
-
+new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
 
 // Створюю розмітку для картки як шаблон
 function createGalleryCardsMarkup(galleryItems)  {
